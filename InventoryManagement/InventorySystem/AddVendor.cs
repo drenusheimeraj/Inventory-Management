f7de071ff5019bf5fc959f4636feb3ee7e9fc697
Drenushe Imeraj <drenusha.imeraj@gmail.com>
2018-03-19 15:13:20 +0100

    Add Vendor

---
+++ InventoryManagement/InventorySystem/AddVendor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class AddVendor : Form
+    {
+
+        private class VendorImpl : VendorInterface
+        {
+            public void onVendorAdd(bool success)
+            {
+                MessageBox.Show("Vendor Added");
+            }
+        }
+
+        private VendorImpl vendorImpl;
+        public AddVendor()
+        {
+            InitializeComponent();
+            vendorImpl = new VendorImpl();
+        }
+
+        private void AddVendor_Load(object sender, EventArgs e)
+        {
+            DataTable dt = ClassInventory.Instance.ProductList();
+            dg.DataSource = dt;
+            for (int i = 0; i < dg.Rows.Count - 1; i++)
+            { 
+           cbProduct.Items.Add(dg.Rows[i].Cells[0].Value);
+            }
+
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            string Name=tbName.Text;
+            string Email=tbEmail.Text;
+            string Address=tbAddress.Text;
+            string Product=cbProduct.Text;
+            int val = ClassInventory.Instance.InsertVendor(Name, Email, Address, Product);
+            if (val > 0)
+            {
+                vendorImpl.onVendorAdd(true);
+            }
+        }
+    }
+}
---
+++ InventoryManagement/InventorySystem/AddVendor.designer.cs
@@ -0,0 +1,178 @@
+﻿namespace InventorySystem
+{
+    partial class AddVendor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbProduct = new System.Windows.Forms.ComboBox();
+            this.lblProduct = new System.Windows.Forms.Label();
+            this.btClose = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.dg = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbProduct
+            // 
+            this.cbProduct.FormattingEnabled = true;
+            this.cbProduct.Location = new System.Drawing.Point(163, 222);
+            this.cbProduct.Name = "cbProduct";
+            this.cbProduct.Size = new System.Drawing.Size(220, 21);
+            this.cbProduct.TabIndex = 58;
+            // 
+            // lblProduct
+            // 
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.Location = new System.Drawing.Point(92, 222);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(44, 13);
+            this.lblProduct.TabIndex = 57;
+            this.lblProduct.Text = "Product";
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(341, 289);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 56;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(163, 289);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 55;
+            this.btAdd.Text = "ADD";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(163, 107);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(197, 20);
+            this.tbEmail.TabIndex = 54;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(163, 137);
+            this.tbAddress.Multiline = true;
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(340, 70);
+            this.tbAddress.TabIndex = 53;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(92, 152);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 52;
+            this.lblAddress.Text = "Address";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(163, 76);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(197, 20);
+            this.tbName.TabIndex = 51;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(92, 114);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 50;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(82, 83);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 49;
+            this.lblName.Text = "Name";
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(12, 270);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(67, 42);
+            this.dg.TabIndex = 59;
+            this.dg.Visible = false;
+            // 
+            // AddVendor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 519);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.cbProduct);
+            this.Controls.Add(this.lblProduct);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Name = "AddVendor";
+            this.Text = "Name";
+            this.Load += new System.EventHandler(this.AddVendor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbProduct;
+        private System.Windows.Forms.Label lblProduct;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.DataGridView dg;
+    }
+}
