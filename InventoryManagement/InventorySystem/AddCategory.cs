2dcbc44856e9964f9c54683088217fe5225a7628
Fllanza Avdimetaj <fllanzaavdimetaj@gmail.com>
2018-03-19 15:20:56 +0100

    Add Category

---
+++ InventoryManagement/InventorySystem/AddCategory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class AddCategory : Form
+    {
+        public AddCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            int val = ClassInventory.Instance.InsertCategory(tbCategory.Text);
+            if (val > 0)
+            {
+                MessageBox.Show("Category Added");
+            }
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ InventoryManagement/InventorySystem/AddCategory.designer.cs
@@ -0,0 +1,96 @@
+﻿namespace InventorySystem
+{
+    partial class AddCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblcategory = new System.Windows.Forms.Label();
+            this.tbCategory = new System.Windows.Forms.TextBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblcategory
+            // 
+            this.lblcategory.AutoSize = true;
+            this.lblcategory.Location = new System.Drawing.Point(63, 71);
+            this.lblcategory.Name = "lblcategory";
+            this.lblcategory.Size = new System.Drawing.Size(80, 13);
+            this.lblcategory.TabIndex = 0;
+            this.lblcategory.Text = "Category Name";
+            // 
+            // tbCategory
+            // 
+            this.tbCategory.Location = new System.Drawing.Point(149, 68);
+            this.tbCategory.Name = "tbCategory";
+            this.tbCategory.Size = new System.Drawing.Size(205, 20);
+            this.tbCategory.TabIndex = 1;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(149, 118);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(260, 118);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 3;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // AddCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 182);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.tbCategory);
+            this.Controls.Add(this.lblcategory);
+            this.Name = "AddCategory";
+            this.Text = "AddCategory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblcategory;
+        private System.Windows.Forms.TextBox tbCategory;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btClose;
+    }
+}
