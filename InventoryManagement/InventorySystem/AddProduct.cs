17353784d442fbb20c90c7ebf77a60d0e6087188
Agnesa Selimi <agnesa.selimi@gmail.com>
2018-03-19 15:30:04 +0100

    Add Product

---
+++ InventoryManagement/InventorySystem/AddProduct.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class AddProduct : Form
+    {
+        private ProductImpl prouctImpl;
+        class ProductImpl : ProductInterface
+        {
+            public void onAddProduct(bool success)
+            {
+                Console.WriteLine("success " + success);
+            }
+        }
+        public AddProduct()
+        {
+            InitializeComponent();
+            prouctImpl = new ProductImpl();
+
+
+        }
+
+        private void AddProduct_Load(object sender, EventArgs e)
+        {
+            DataTable dt = ClassInventory.Instance.ProductCategoryList();
+           
+            dg.DataSource = dt;
+            for (int i = 0; i < dg.Rows.Count-1; i++)
+            {
+                cbCategory.Items.Add(dg.Rows[i].Cells[0].Value);
+            }
+            
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            string Name = tbName.Text;
+            string Cost = tbCost.Text;
+            string Desc = tbDesc.Text;
+            string Category = cbCategory.Text;
+            int val = ClassInventory.Instance.InsertProduct(Name, Cost, Desc, Category);
+            if (val > 0)
+            {
+                prouctImpl.onAddProduct(true);
+                MessageBox.Show("Product Added");
+            }
+        }
+    }
+}
---
+++ InventoryManagement/InventorySystem/AddProduct.designer.cs
@@ -0,0 +1,182 @@
+﻿namespace InventorySystem
+{
+    partial class AddProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btClose = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.tbCost = new System.Windows.Forms.TextBox();
+            this.tbDesc = new System.Windows.Forms.TextBox();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.lblCost = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
+            this.dg = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(438, 284);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 46;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(260, 284);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 45;
+            this.btAdd.Text = "ADD";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // tbCost
+            // 
+            this.tbCost.Location = new System.Drawing.Point(260, 102);
+            this.tbCost.Name = "tbCost";
+            this.tbCost.Size = new System.Drawing.Size(197, 20);
+            this.tbCost.TabIndex = 44;
+            this.tbCost.Text = "1111";
+            // 
+            // tbDesc
+            // 
+            this.tbDesc.Location = new System.Drawing.Point(260, 132);
+            this.tbDesc.Multiline = true;
+            this.tbDesc.Name = "tbDesc";
+            this.tbDesc.Size = new System.Drawing.Size(340, 70);
+            this.tbDesc.TabIndex = 43;
+            this.tbDesc.Text = "GGGG";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(189, 147);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(60, 13);
+            this.lblDesc.TabIndex = 42;
+            this.lblDesc.Text = "Description";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(260, 71);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(197, 20);
+            this.tbName.TabIndex = 40;
+            this.tbName.Text = "Lenovo12";
+            // 
+            // lblCost
+            // 
+            this.lblCost.AutoSize = true;
+            this.lblCost.Location = new System.Drawing.Point(189, 109);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(28, 13);
+            this.lblCost.TabIndex = 36;
+            this.lblCost.Text = "Cost";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(179, 78);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(75, 13);
+            this.lblName.TabIndex = 35;
+            this.lblName.Text = "Product Name";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(189, 217);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(49, 13);
+            this.lblCategory.TabIndex = 47;
+            this.lblCategory.Text = "Category";
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(260, 217);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(220, 21);
+            this.cbCategory.TabIndex = 48;
+            this.cbCategory.Text = "Computers";
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(38, 78);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(66, 35);
+            this.dg.TabIndex = 49;
+            this.dg.Visible = false;
+            // 
+            // AddProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 493);
+            this.Controls.Add(this.dg);
+            this.Controls.Add(this.cbCategory);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.tbCost);
+            this.Controls.Add(this.tbDesc);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.lblName);
+            this.Name = "AddProduct";
+            this.Text = "AddProduct";
+            this.Load += new System.EventHandler(this.AddProduct_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.TextBox tbCost;
+        private System.Windows.Forms.TextBox tbDesc;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cbCategory;
+        private System.Windows.Forms.DataGridView dg;
+    }
+}
