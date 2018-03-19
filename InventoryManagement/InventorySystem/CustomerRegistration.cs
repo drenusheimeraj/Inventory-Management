d605cd90931dcd686b04de0abe4e529d929e63a6
Fllanza Avdimetaj <fllanzaavdimetaj@gmail.com>
2018-03-19 15:55:22 +0100

    Add CostumerRegistration

---
+++ InventoryManagement/InventorySystem/CustomerRegistration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class CustomerRegistration : Form
+    {
+        public CustomerRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            string Name = tbName.Text;
+            string Date = dtpBirth.Text;
+            string Email = tbEmail.Text;
+            string Gender = cbGender.Text;
+            string Address = tbAddress.Text;
+            int val = ClassInventory.Instance.InsertCustomer(Name, Date, Email, Gender, Address);
+            if (val > 0)
+            {
+                MessageBox.Show("Customer has been Registered");
+                tbName.Text = "";
+                tbEmail.Text="";
+                tbAddress.Text = "";
+
+            }
+        }
+    }
+}
---
+++ InventoryManagement/InventorySystem/CustomerRegistration.designer.cs
@@ -0,0 +1,188 @@
+﻿namespace InventorySystem
+{
+    partial class CustomerRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.dtpBirth = new System.Windows.Forms.DateTimePicker();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(278, 146);
+            this.tbAddress.Multiline = true;
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(340, 70);
+            this.tbAddress.TabIndex = 31;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(207, 149);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 30;
+            this.lblAddress.Text = "Address";
+            // 
+            // dtpBirth
+            // 
+            this.dtpBirth.Location = new System.Drawing.Point(278, 61);
+            this.dtpBirth.Name = "dtpBirth";
+            this.dtpBirth.Size = new System.Drawing.Size(200, 20);
+            this.dtpBirth.TabIndex = 29;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(278, 31);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(197, 20);
+            this.tbName.TabIndex = 27;
+            // 
+            // cbGender
+            // 
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Items.AddRange(new object[] {
+            "Female",
+            "Male"});
+            this.cbGender.Location = new System.Drawing.Point(278, 119);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(200, 21);
+            this.cbGender.TabIndex = 25;
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(207, 123);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(42, 13);
+            this.lblGender.TabIndex = 19;
+            this.lblGender.Text = "Gender";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(207, 95);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(46, 13);
+            this.lblEmail.TabIndex = 18;
+            this.lblEmail.Text = "Email ID";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(207, 69);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(66, 13);
+            this.lblDate.TabIndex = 17;
+            this.lblDate.Text = "Date of Birth";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(207, 38);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 16;
+            this.lblName.Text = "Name";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(281, 92);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(197, 20);
+            this.tbEmail.TabIndex = 32;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(278, 244);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 33;
+            this.btAdd.Text = "ADD";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(456, 244);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 34;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            // 
+            // CustomerRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 518);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.dtpBirth);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblName);
+            this.Name = "CustomerRegistration";
+            this.Text = "CustomerRegistration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.DateTimePicker dtpBirth;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btClose;
+    }
+}
