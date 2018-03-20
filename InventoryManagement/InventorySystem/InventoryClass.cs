639e0effaf689289199e673fb4fdc95891514b99
Drenushe Imeraj <drenusha.imeraj@gmail.com>
2018-03-20 16:41:16 +0100

    Add InventoryClass

---
+++ InventoryManagement/InventorySystem/InventoryClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Text.RegularExpressions;
+using System.Data.Sql;
+using System.Data.SqlClient;
+namespace InventorySystem
+{
+    class InventoryClass
+    {
+        public static DataTable StaffTable()
+        {
+            string Query = "Select * from StaffTable";
+            SqlCommand sc = new SqlCommand(Query, ConnectionString.GetConnection());
+            DataTable dt = new DataTable();
+            SqlDataAdapter sds = new SqlDataAdapter(sc);
+            sds.Fill(dt);
+            return dt;
+        }
+    }
+}
