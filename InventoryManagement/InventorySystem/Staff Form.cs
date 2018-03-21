a061c24db32ce371b2ad9a15876d7cb07b46500d
Agnesa Selimi <agnesa.selimi@gmail.com>
2018-03-21 16:38:26 +0100

    Add Staff

---
+++ InventoryManagement/InventorySystem/Staff Form.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class StaffForm : Form
+    {
+        public StaffForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StaffForm_Load(object sender, EventArgs e)
+        {
+            DataTable dt = InventoryClass.StaffTable();
+            dgStaffInfo.DataSource = dt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ InventoryManagement/InventorySystem/Staff Form.designer.cs
@@ -0,0 +1,226 @@
+﻿namespace InventorySystem
+{
+    partial class StaffForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblContact = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbContact = new System.Windows.Forms.TextBox();
+            this.cbPosition = new System.Windows.Forms.ComboBox();
+            this.dgStaffInfo = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgStaffInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(19, 239);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 0;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Location = new System.Drawing.Point(100, 239);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btUpdate.TabIndex = 1;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(531, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(184, 239);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 3;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(366, 17);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 4;
+            this.lblID.Text = "ID";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(42, 74);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "Name";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(42, 107);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 6;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblContact
+            // 
+            this.lblContact.AutoSize = true;
+            this.lblContact.Location = new System.Drawing.Point(42, 138);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(44, 13);
+            this.lblContact.TabIndex = 7;
+            this.lblContact.Text = "Contact";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(42, 169);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(44, 13);
+            this.lblPosition.TabIndex = 8;
+            this.lblPosition.Text = "Position";
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(425, 14);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(100, 20);
+            this.tbID.TabIndex = 9;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(101, 74);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 10;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(101, 107);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(100, 20);
+            this.tbAddress.TabIndex = 11;
+            // 
+            // tbContact
+            // 
+            this.tbContact.Location = new System.Drawing.Point(101, 138);
+            this.tbContact.Name = "tbContact";
+            this.tbContact.Size = new System.Drawing.Size(100, 20);
+            this.tbContact.TabIndex = 12;
+            // 
+            // cbPosition
+            // 
+            this.cbPosition.FormattingEnabled = true;
+            this.cbPosition.Items.AddRange(new object[] {
+            "Position1",
+            "Position2",
+            "Position3"});
+            this.cbPosition.Location = new System.Drawing.Point(101, 169);
+            this.cbPosition.Name = "cbPosition";
+            this.cbPosition.Size = new System.Drawing.Size(121, 21);
+            this.cbPosition.TabIndex = 13;
+            // 
+            // dgStaffInfo
+            // 
+            this.dgStaffInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgStaffInfo.Location = new System.Drawing.Point(366, 59);
+            this.dgStaffInfo.Name = "dgStaffInfo";
+            this.dgStaffInfo.Size = new System.Drawing.Size(240, 254);
+            this.dgStaffInfo.TabIndex = 14;
+            // 
+            // StaffForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 363);
+            this.Controls.Add(this.dgStaffInfo);
+            this.Controls.Add(this.cbPosition);
+            this.Controls.Add(this.tbContact);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.lblContact);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.btAdd);
+            this.Name = "StaffForm";
+            this.Text = "Staff Form";
+            this.Load += new System.EventHandler(this.StaffForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgStaffInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblContact;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbContact;
+        private System.Windows.Forms.ComboBox cbPosition;
+        private System.Windows.Forms.DataGridView dgStaffInfo;
+    }
+}
