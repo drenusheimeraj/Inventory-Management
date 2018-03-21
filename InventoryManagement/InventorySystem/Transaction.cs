6bc25b8545c39f53d01f9fc709e0baae54e3beaf
Fllanza Avdimetaj <fllanzaavdimetaj@gmail.com>
2018-03-21 16:43:05 +0100

    Add Transaction

---
+++ InventoryManagement/InventorySystem/Transaction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class Transaction : Form
+    {
+        public Transaction()
+        {
+            InitializeComponent();
+        }
+
+        private void Transaction_Load(object sender, EventArgs e)
+        {
+            DataTable dt = ClassInventory.Instance.TableCustomer();
+            dgCustomer.DataSource = dt;
+            for (int i = 0; i < dgCustomer.Rows.Count - 1; i++)
+            {
+                cbCustomer.Items.Add(dgCustomer.Rows[i].Cells[1].Value);
+            }
+            DataTable dt1 = ClassInventory.Instance.ProductTable();
+            dgProduct.DataSource = dt1;
+            for (int j = 0; j < dgProduct.Rows.Count - 1; j++)
+            {
+                cbProduct.Items.Add(dgProduct.Rows[j].Cells[1].Value);
+            }
+        }
+    }
+}
---
+++ InventoryManagement/InventorySystem/Transaction.designer.cs
@@ -0,0 +1,258 @@
+﻿namespace InventorySystem
+{
+    partial class Transaction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblProduct = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.cbCustomer = new System.Windows.Forms.ComboBox();
+            this.cbProduct = new System.Windows.Forms.ComboBox();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.tbQuantity = new System.Windows.Forms.TextBox();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblDescp = new System.Windows.Forms.Label();
+            this.tbDesc = new System.Windows.Forms.TextBox();
+            this.btConfirm = new System.Windows.Forms.Button();
+            this.btClose = new System.Windows.Forms.Button();
+            this.dgCustomer = new System.Windows.Forms.DataGridView();
+            this.dgProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(139, 85);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(51, 13);
+            this.lblCustomerName.TabIndex = 0;
+            this.lblCustomerName.Text = "Customer";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(139, 116);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "Date";
+            // 
+            // lblProduct
+            // 
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.Location = new System.Drawing.Point(139, 138);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(44, 13);
+            this.lblProduct.TabIndex = 3;
+            this.lblProduct.Text = "Product";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(142, 164);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(31, 13);
+            this.lblPrice.TabIndex = 4;
+            this.lblPrice.Text = "Price";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(140, 190);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(46, 13);
+            this.lblQuantity.TabIndex = 5;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(142, 222);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "Total";
+            // 
+            // cbCustomer
+            // 
+            this.cbCustomer.FormattingEnabled = true;
+            this.cbCustomer.Location = new System.Drawing.Point(210, 82);
+            this.cbCustomer.Name = "cbCustomer";
+            this.cbCustomer.Size = new System.Drawing.Size(200, 21);
+            this.cbCustomer.TabIndex = 7;
+            // 
+            // cbProduct
+            // 
+            this.cbProduct.FormattingEnabled = true;
+            this.cbProduct.Location = new System.Drawing.Point(210, 134);
+            this.cbProduct.Name = "cbProduct";
+            this.cbProduct.Size = new System.Drawing.Size(200, 21);
+            this.cbProduct.TabIndex = 9;
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(213, 161);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.ReadOnly = true;
+            this.tbPrice.Size = new System.Drawing.Size(197, 20);
+            this.tbPrice.TabIndex = 10;
+            // 
+            // tbQuantity
+            // 
+            this.tbQuantity.Location = new System.Drawing.Point(213, 187);
+            this.tbQuantity.Name = "tbQuantity";
+            this.tbQuantity.Size = new System.Drawing.Size(197, 20);
+            this.tbQuantity.TabIndex = 11;
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Location = new System.Drawing.Point(213, 222);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.ReadOnly = true;
+            this.tbTotal.Size = new System.Drawing.Size(197, 20);
+            this.tbTotal.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(210, 108);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // lblDescp
+            // 
+            this.lblDescp.AutoSize = true;
+            this.lblDescp.Location = new System.Drawing.Point(142, 266);
+            this.lblDescp.Name = "lblDescp";
+            this.lblDescp.Size = new System.Drawing.Size(60, 13);
+            this.lblDescp.TabIndex = 14;
+            this.lblDescp.Text = "Description";
+            // 
+            // tbDesc
+            // 
+            this.tbDesc.Location = new System.Drawing.Point(213, 263);
+            this.tbDesc.Multiline = true;
+            this.tbDesc.Name = "tbDesc";
+            this.tbDesc.Size = new System.Drawing.Size(340, 70);
+            this.tbDesc.TabIndex = 15;
+            // 
+            // btConfirm
+            // 
+            this.btConfirm.Location = new System.Drawing.Point(653, 298);
+            this.btConfirm.Name = "btConfirm";
+            this.btConfirm.Size = new System.Drawing.Size(75, 23);
+            this.btConfirm.TabIndex = 16;
+            this.btConfirm.Text = "Confirm";
+            this.btConfirm.UseVisualStyleBackColor = true;
+            // 
+            // btClose
+            // 
+            this.btClose.Location = new System.Drawing.Point(734, 298);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(75, 23);
+            this.btClose.TabIndex = 17;
+            this.btClose.Text = "Close";
+            this.btClose.UseVisualStyleBackColor = true;
+            // 
+            // dgCustomer
+            // 
+            this.dgCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCustomer.Location = new System.Drawing.Point(572, 12);
+            this.dgCustomer.Name = "dgCustomer";
+            this.dgCustomer.Size = new System.Drawing.Size(77, 49);
+            this.dgCustomer.TabIndex = 18;
+            // 
+            // dgProduct
+            // 
+            this.dgProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProduct.Location = new System.Drawing.Point(572, 67);
+            this.dgProduct.Name = "dgProduct";
+            this.dgProduct.Size = new System.Drawing.Size(77, 49);
+            this.dgProduct.TabIndex = 19;
+            // 
+            // Transaction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 403);
+            this.Controls.Add(this.dgProduct);
+            this.Controls.Add(this.dgCustomer);
+            this.Controls.Add(this.btClose);
+            this.Controls.Add(this.btConfirm);
+            this.Controls.Add(this.tbDesc);
+            this.Controls.Add(this.lblDescp);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.tbTotal);
+            this.Controls.Add(this.tbQuantity);
+            this.Controls.Add(this.tbPrice);
+            this.Controls.Add(this.cbProduct);
+            this.Controls.Add(this.cbCustomer);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblProduct);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblCustomerName);
+            this.Name = "Transaction";
+            this.Text = "Transaction";
+            this.Load += new System.EventHandler(this.Transaction_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProduct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblProduct;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.ComboBox cbCustomer;
+        private System.Windows.Forms.ComboBox cbProduct;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.TextBox tbQuantity;
+        private System.Windows.Forms.TextBox tbTotal;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblDescp;
+        private System.Windows.Forms.TextBox tbDesc;
+        private System.Windows.Forms.Button btConfirm;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.DataGridView dgCustomer;
+        private System.Windows.Forms.DataGridView dgProduct;
+    }
+}
