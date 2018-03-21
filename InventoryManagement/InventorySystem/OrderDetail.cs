80ff68b4bb6436c99faa78709cb1d0ad4b50e18f
Fllanza Avdimetaj <fllanzaavdimetaj@gmail.com>
2018-03-21 13:43:44 +0100

    Add OrderDetail

---
+++ InventoryManagement/InventorySystem/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class OrderDetail : Form
+    {
+        public OrderDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
