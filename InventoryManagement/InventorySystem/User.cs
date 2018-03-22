9938c5d95cdbd896be748f5e4e87f38838f484ed
Drenushe Imeraj <drenusha.imeraj@gmail.com>
2018-03-22 13:38:43 +0100

    Add User Helper & Exception

---
+++ InventoryManagement/InventorySystem/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InventorySystem
+{
+    abstract class User : Person
+    {
+    
+        public string username;
+        public string password;
+
+    }
+
+}
---
+++ InventoryManagement/InventorySystem/UserHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InventorySystem
+{
+    static class UserHelper
+    {
+        public static User user;
+    }
+}
