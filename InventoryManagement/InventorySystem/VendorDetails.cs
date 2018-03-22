5594337a75b3dc6a4d32057d9d98e3470d29cc5f
Agnesa Selimi <agnesa.selimi@gmail.com>
2018-03-22 13:45:19 +0100

    Add VendorDetails

---
+++ InventoryManagement/InventorySystem/VendorDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class VendorDetails : Form
+    {
+        public VendorDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void VendorDetails_Load(object sender, EventArgs e)
+        {
+            DataTable dt = ClassInventory.Instance.VendorTable();
+            dg.DataSource = dt;
+        }
+    }
+}
---
+++ InventoryManagement/InventorySystem/VendorDetails.designer.cs
@@ -0,0 +1,61 @@
+﻿namespace InventorySystem
+{
+    partial class VendorDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dg = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dg
+            // 
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Location = new System.Drawing.Point(91, 39);
+            this.dg.Name = "dg";
+            this.dg.Size = new System.Drawing.Size(451, 381);
+            this.dg.TabIndex = 0;
+            // 
+            // VendorDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 486);
+            this.Controls.Add(this.dg);
+            this.Name = "VendorDetails";
+            this.Text = "VendorDetails";
+            this.Load += new System.EventHandler(this.VendorDetails_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dg;
+    }
+}
